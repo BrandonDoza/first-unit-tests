7f3307ba9b5bbf4d667341002f637e42d9c7c6e1
Brandon Doza <brandondoza@brandos-m2-mbp.lan>
2024-02-04 15:35:50 -0700

    finsih the unit test

--- functions.js
+++ functions.js
@@ -1,9 +1,32 @@
-// functions.js
-function addTwoNumbers() {
+function addTwoNumbers(num1, num2) {
+    var sum = 0;
+    sum = num1 + num2;
+    return sum;
 }
+
+function sayHello(name) {
+    if (name === 'Will') {
+        return `No more testing ${name}!`;
+    }
+    return `Hi there ${name}!`;
+}
+
+function buildCar(color, type) {
+    if (color === undefined && type === undefined) {
+        return {}
+    } else {
+    return {
+        color: color,
+        type: type,
+    } 
+    }
+}
+
 
 module.exports = {
     addTwoNumbers,
-    // sayHello,
-    // buildCar
+    sayHello,
+    buildCar
 }
+
+
